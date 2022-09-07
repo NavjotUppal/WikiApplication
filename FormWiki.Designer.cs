119710348e2e9790a011161d9a37d51cb6145af4
NAVJOT KAUR <uppal.navjot25@gmail.com>
2022-09-07 11:19:24 +0800

    GUI completed.

--- FormWiki.Designer.cs
+++ FormWiki.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.textBoxName = new System.Windows.Forms.TextBox();
             this.textBoxDefinition = new System.Windows.Forms.TextBox();
@@ -41,13 +40,20 @@
             this.buttonADD = new System.Windows.Forms.Button();
             this.buttonDEL = new System.Windows.Forms.Button();
             this.buttonEDIT = new System.Windows.Forms.Button();
+            this.buttonLOAD = new System.Windows.Forms.Button();
+            this.buttonSAVE = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.NAME = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CATEGORY = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonSEARCH = new System.Windows.Forms.Button();
+            this.textBoxSEARCH = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Location = new System.Drawing.Point(13, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 0;
@@ -56,25 +62,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Location = new System.Drawing.Point(9, 63);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(62, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Category";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 104);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(59, 16);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Structure";
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 176);
+            this.label4.Location = new System.Drawing.Point(12, 185);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(59, 16);
             this.label4.TabIndex = 3;
@@ -82,14 +79,14 @@
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(90, 31);
+            this.textBoxName.Location = new System.Drawing.Point(90, 19);
             this.textBoxName.Name = "textBoxName";
             this.textBoxName.Size = new System.Drawing.Size(203, 22);
             this.textBoxName.TabIndex = 4;
             // 
             // textBoxDefinition
             // 
-            this.textBoxDefinition.Location = new System.Drawing.Point(90, 176);
+            this.textBoxDefinition.Location = new System.Drawing.Point(90, 185);
             this.textBoxDefinition.Multiline = true;
             this.textBoxDefinition.Name = "textBoxDefinition";
             this.textBoxDefinition.Size = new System.Drawing.Size(203, 194);
@@ -107,16 +104,17 @@
             // 
             this.groupBox1.Controls.Add(this.radioButtonNonLinear);
             this.groupBox1.Controls.Add(this.radioButtonLinear);
-            this.groupBox1.Location = new System.Drawing.Point(90, 93);
+            this.groupBox1.Location = new System.Drawing.Point(16, 107);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(178, 72);
+            this.groupBox1.Size = new System.Drawing.Size(306, 58);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Structure";
             // 
             // radioButtonLinear
             // 
             this.radioButtonLinear.AutoSize = true;
-            this.radioButtonLinear.Location = new System.Drawing.Point(6, 21);
+            this.radioButtonLinear.Location = new System.Drawing.Point(74, 21);
             this.radioButtonLinear.Name = "radioButtonLinear";
             this.radioButtonLinear.Size = new System.Drawing.Size(65, 20);
             this.radioButtonLinear.TabIndex = 0;
@@ -127,7 +125,7 @@
             // radioButtonNonLinear
             // 
             this.radioButtonNonLinear.AutoSize = true;
-            this.radioButtonNonLinear.Location = new System.Drawing.Point(6, 47);
+            this.radioButtonNonLinear.Location = new System.Drawing.Point(183, 21);
             this.radioButtonNonLinear.Name = "radioButtonNonLinear";
             this.radioButtonNonLinear.Size = new System.Drawing.Size(94, 20);
             this.radioButtonNonLinear.TabIndex = 1;
@@ -162,11 +160,73 @@
             this.buttonEDIT.Text = "EDIT";
             this.buttonEDIT.UseVisualStyleBackColor = true;
             // 
+            // buttonLOAD
+            // 
+            this.buttonLOAD.Location = new System.Drawing.Point(458, 400);
+            this.buttonLOAD.Name = "buttonLOAD";
+            this.buttonLOAD.Size = new System.Drawing.Size(75, 23);
+            this.buttonLOAD.TabIndex = 11;
+            this.buttonLOAD.Text = "LOAD";
+            this.buttonLOAD.UseVisualStyleBackColor = true;
+            // 
+            // buttonSAVE
+            // 
+            this.buttonSAVE.Location = new System.Drawing.Point(612, 400);
+            this.buttonSAVE.Name = "buttonSAVE";
+            this.buttonSAVE.Size = new System.Drawing.Size(75, 23);
+            this.buttonSAVE.TabIndex = 12;
+            this.buttonSAVE.Text = "SAVE";
+            this.buttonSAVE.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NAME,
+            this.CATEGORY});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(423, 63);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(315, 316);
+            this.listView1.TabIndex = 13;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // NAME
+            // 
+            this.NAME.Text = "NAME";
+            this.NAME.Width = 112;
+            // 
+            // CATEGORY
+            // 
+            this.CATEGORY.Text = "CATEGORY";
+            this.CATEGORY.Width = 137;
+            // 
+            // buttonSEARCH
+            // 
+            this.buttonSEARCH.Location = new System.Drawing.Point(636, 19);
+            this.buttonSEARCH.Name = "buttonSEARCH";
+            this.buttonSEARCH.Size = new System.Drawing.Size(102, 23);
+            this.buttonSEARCH.TabIndex = 14;
+            this.buttonSEARCH.Text = "SEARCH";
+            this.buttonSEARCH.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSEARCH
+            // 
+            this.textBoxSEARCH.Location = new System.Drawing.Point(423, 19);
+            this.textBoxSEARCH.Name = "textBoxSEARCH";
+            this.textBoxSEARCH.Size = new System.Drawing.Size(187, 22);
+            this.textBoxSEARCH.TabIndex = 15;
+            // 
             // FormWiki
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxSEARCH);
+            this.Controls.Add(this.buttonSEARCH);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.buttonSAVE);
+            this.Controls.Add(this.buttonLOAD);
             this.Controls.Add(this.buttonEDIT);
             this.Controls.Add(this.buttonDEL);
             this.Controls.Add(this.buttonADD);
@@ -175,7 +235,6 @@
             this.Controls.Add(this.textBoxDefinition);
             this.Controls.Add(this.textBoxName);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "FormWiki";
@@ -191,7 +250,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBoxName;
         private System.Windows.Forms.TextBox textBoxDefinition;
@@ -202,6 +260,13 @@
         private System.Windows.Forms.Button buttonADD;
         private System.Windows.Forms.Button buttonDEL;
         private System.Windows.Forms.Button buttonEDIT;
+        private System.Windows.Forms.Button buttonLOAD;
+        private System.Windows.Forms.Button buttonSAVE;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader NAME;
+        private System.Windows.Forms.ColumnHeader CATEGORY;
+        private System.Windows.Forms.Button buttonSEARCH;
+        private System.Windows.Forms.TextBox textBoxSEARCH;
     }
 }
 
